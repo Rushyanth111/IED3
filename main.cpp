6862d4d1774d5ca7bad95fed3a06e156172b3d8c
XavierSchiller <22575255+XavierSchiller@users.noreply.github.com>
2018-07-30 23:28:24 +0530

    Setting up.

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+/******************************************************
+    Main.cpp is used for parsing the given file and 
+    passing it on to the function. Do not use this 
+    file for anything beying making passing on the
+    parsed file name to the actual backend.
+******************************************************/
+int main()
+{
+    std::cout<<"Hello World";
+    return 0;
+}
